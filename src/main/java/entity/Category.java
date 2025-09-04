4af7e0810cb1f67ad89c2dc52a8c86fbcb845970
nguyenit24 <nguyenhuynh.463459@gmail.com>
2025-09-04 21:00:38 +0700

    Complete

---
+++ src/main/java/entity/Category.java
@@ -0,0 +1,57 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+@NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c")
+public class Category {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+	 
+	@Column(name = "categoryname",columnDefinition = "nvarchar(50)")
+	private String categoryname;
+	
+	private String images;
+
+	public Category(int id, String categoryname, String images) {
+		super();
+		this.id = id;
+		this.categoryname = categoryname;
+		this.images = images;
+	}
+	public Category() {
+		
+	}
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryname() {
+		return categoryname;
+	}
+
+	public void setCategoryname(String categoryname) {
+		this.categoryname = categoryname;
+	}
+
+	public String getImages() {
+		return images;
+	}
+
+	public void setImages(String images) {
+		this.images = images;
+	}
+	
+}
